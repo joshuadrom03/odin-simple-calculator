6253af2d75cfad4da5cd28934b8f770b04bc705c
Joshua <187735522+joshuadrom03@users.noreply.github.com>
2024-11-19 13:38:54 +0200

    added the html,and JS basic functions for addition,subtraction, multiple and divide

---
+++ script.js
@@ -0,0 +1,29 @@
+let num1 = 10;
+let num2= 5;
+let operator = "";
+
+
+function addNums(){
+    return num1 + num2;
+
+}
+
+function subtractNums(){
+    return num1 - num2;
+
+}
+
+function multipleNums(){
+    return num1 * num2;
+
+}
+
+function divideNums(){
+    return num1 / num2;
+
+}
+
+console.log("Addition: " + addNums());
+console.log("Subtraction: " + subtractNums());
+console.log("Multiply: " + multipleNums());
+console.log("Divide: " + divideNums());

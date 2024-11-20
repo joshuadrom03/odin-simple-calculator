712470acba22cc41ab79ff39d536ac1e1dff3335
Joshua <187735522+joshuadrom03@users.noreply.github.com>
2024-11-20 11:35:58 +0200

    added the button functionality for the numbers and the operators and the calculate function to the equal button and displayed it to the screen

--- script.js
+++ script.js
@@ -1,47 +1,77 @@
-let num1 = 10;
-let num2= 5;
-let operator = "/";
+let operator ="";
+let previousValue ="";
+let currentValue = "";
+
+document.addEventListener("DOMContentLoaded", function(){
+    //Store the components
+    let clear = document.querySelector("#clear");
+    let equal = document.querySelector("#equals");
+    let numbers = document.querySelectorAll(".nums");
+    let operators = document.querySelectorAll(".operator");
+    let screen = document.querySelector(".display");
+
+    numbers.forEach((number) => number.addEventListener("click", function(e){
+        handleNumber(e.target.textContent)
+        screen.textContent = currentValue;
+
+    }))
+
+    operators.forEach((op) => op.addEventListener("click", function(e){
+        handleOperator(e.target.textContent);
+        screen.textContent = previousValue + " " + operator;
 
 
-function addNums(){
-    return num1 + num2;
+    }))
 
-}
+    clear.addEventListener("click", function(){
+        previousValue = "";
+        currentValue = "";
+        operator = "";
+        screen.textContent = "00";
+    })
 
-function subtractNums(){
-    return num1 - num2;
+    equal.addEventListener("click", function(e){
+        operate();
+        screen.textContent = previousValue;
 
-}
+    })
 
-function multipleNums(){
-    return num1 * num2;
+})
 
-}
-
-function divideNums(){
-    return num1 / num2;
-
-}
-
-function operate(){
-
-    switch(operator){
-        case "+" :
-            console.log(addNums());
-            break;
-        
-        case "-":
-            console.log(subtractNums());
-            break;
-
-        case "*":
-            console.log(multipleNums());
-            break;
-
-        case "/":
-            console.log(divideNums());
-            break;
+function handleNumber(num){
+    if(currentValue.length <= 7){
+        currentValue += num;
     }
 }
 
+function handleOperator(op){
+    operator = op;
+    previousValue = currentValue;
+    currentValue = "";
+}
+
+function operate(){
+    previousValue = Number(previousValue);
+    currentValue = Number(currentValue);
+
+    switch(operator){
+        case "+":
+            previousValue += currentValue;
+            break;
+        
+        case "-":
+            previousValue -= currentValue
+            break;
+
+        case "*":
+            previousValue *= currentValue;
+            break;
+
+        case "/":
+            previousValue /= currentValue;
+            break;
+    }
+
+    previousValue = previousValue.toString();
+}
 
